5c64b7186d095e553b9649ae54cb8886bb497010
JiveshDhakate <jdhakate422@gmail.com>
2026-01-11 16:32:49 +0000

    CRUD Done

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("addTask/",views.addTask, name="addTask"),
+    path("markasComplete/<int:task_id>/",views.markasComplete, name="markasComplete"),
+    path("markasIncomplete/<int:task_id>/",views.markasIncomplete, name="markasIncomplete"),
+    path("deleteTask/<int:task_id>/",views.deleteTask, name="deleteTask"),
+    path("editTask/<int:task_id>/",views.editTask, name="editTask"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from django.http import HttpResponse
 from .models import Task
 
@@ -8,4 +8,32 @@ def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def markasComplete(request,task_id):
+    task=get_object_or_404(Task,id=task_id)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def markasIncomplete(request,task_id):
+    task=get_object_or_404(Task,id=task_id)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+def deleteTask(request,task_id):
+    task=Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('home')
+
+def editTask(request,task_id):
+    get_task=get_object_or_404(Task,id=task_id)
+    if request.method=="POST":
+        updated_task=request.POST['task']
+        get_task.task=updated_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={'get_task':get_task}
+        return render(request,'editTask.html',context)
     
